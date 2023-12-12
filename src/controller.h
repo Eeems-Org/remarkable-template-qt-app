5d0650fb5c809fac0b9cfc547f568af94638351b
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2023-12-11 23:01:45 -0700

    Add more documentation

--- src/controller.h
+++ src/controller.h
@@ -4,7 +4,9 @@
 #include <QTimer>
 #include <QQuickItem>
 #include <epframebuffer.h>
-
+/*!
+ * \brief This class serves as a simple interop between QML and C++
+ */
 class Controller : public QObject {
     Q_OBJECT
 
@@ -12,31 +14,54 @@ public:
     Controller(QObject* parent) : QObject(parent) {}
     ~Controller(){}
 
+    /*!
+     * \brief Method called by QML after it's loaded to allow C++ to start interacting with the UI
+     */
     Q_INVOKABLE void startup(){
         qDebug() << "Running controller startup";
         QTimer::singleShot(10, [this]{
             setState("loaded");
         });
     }
+    /*!
+     * \brief Gets the current state in QML
+     * \return The current state
+     */
     QString state() {
-        if(!getStateControllerUI()){
+        if(stateControllerUI == nullptr && !getStateControllerUI()){
             return "loading";
         }
         return stateControllerUI->property("state").toString();
     }
+    /*!
+     * \brief Sets the current QML state
+     * \param state The state to use
+     */
     void setState(QString state){
-        if(!getStateControllerUI()){
+        if(stateControllerUI == nullptr && !getStateControllerUI()){
             throw "Unable to find state controller";
         }
         stateControllerUI->setProperty("state", state);
     }
-
+    /*!
+     * \brief Change the root QObject for the QML
+     * \param root The root QObject
+     */
     void setRoot(QObject* root){ this->root = root; }
 
 private:
+    /*!
+     * \brief The root QObject for the QML
+     */
     QObject* root = nullptr;
+    /*!
+     * \brief The QML state controller
+     */
     QObject* stateControllerUI = nullptr;
-
+    /*!
+     * \brief Find the QML state controller and cache it's value
+     * \return The QML state controller QObject
+     */
     QObject* getStateControllerUI(){
         stateControllerUI = root->findChild<QObject*>("stateController");
         return stateControllerUI;
--- src/main.cpp
+++ src/main.cpp
@@ -56,8 +56,11 @@ int main(int argc, char *argv[]){
     Controller controller(&app);
     QQmlApplicationEngine engine;
     QQmlContext* context = engine.rootContext();
+    // Expose screenGeometry and controller to QML
     context->setContextProperty("screenGeometry", app.primaryScreen()->geometry());
     context->setContextProperty("controller", &controller);
+    // Load main.qml from the embedded Qt resource file.
+    // This is defined in qml.qrc
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty()){
         qDebug() << "Nothing to display";
@@ -66,6 +69,7 @@ int main(int argc, char *argv[]){
     auto root = engine.rootObjects().first();
     controller.setRoot(root);
 
+    // Setup some signal handlers to make sure to quit the application normally if these signals are recieved
     signal(SIGINT, sigHandler);
     signal(SIGSEGV, sigHandler);
     signal(SIGTERM, sigHandler);
